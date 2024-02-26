0eba8e638a043b099962a8e1ec459055b17b8302
shaporjur <shaporjur@alum.us.es>
2024-02-26 12:52:01 +0100

    cambio finales

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let cool = require("cool-ascii-faces");
 let express = require("express");
 const data = require('./index-SPJ'); 
+const alvaro_data = require('./index-AMD');
+const isabel_data = require('./index-ITR');
 
 
 let app = express();
@@ -41,22 +43,57 @@ function calculateResultSharay(entry, countryWanted) {
 app.get("/samples/SPJ", (req, res) => {
 
     const result = calculateResultSharay(data, "Spain"); 
-    res.send(`The average cumulative annual prefinancing for the country is: ${result}`);
+    res.send(` <html> <body> <h1> The average cumulative annual prefinancing for the country is:  ${result}</h1> </body> </html>`);
 });
 
 
-// .../samples/AMD Request
+
+//Alvaro Algorithm
+function popularity_rate_average(data, country){
+    var list = data.filter((n) => n.country === country).map((n) => n.popularity_rate);
+    
+    
+    if (list.length === 0) {
+        return 0; 
+    }
+    
+    var sum = 0;
+    list.forEach((n) => sum += n);
+
+    var average = sum / list.length;
+
+    return average;
+} 
+
+//Alvaro Sample Request
 app.get("/samples/AMD", (req, res) => {
 
-    const result = popularity_rate_average(amd_data, "Luxembourg"); 
+    const result = popularity_rate_average(alvaro_data, "Luxembourg"); 
     res.send(`<html> <body> <h1> Popularity rate in the country is:  ${result}</h1> </body> </html>`)
     
 });
 
-// .../samples/ITR Request
+
+//Isabel Algorithm
+function averageRecoveredExpensesByCountry(dataEntry, country){
+    let list = dataEntry.filter((data) => data.ms_name.match(country)).map((line) => line.recovery_of_expense)
+    
+    if (list.length === 0) {
+        console.log(`There is no data for the country you have chosen`);
+        return null;
+    }
+
+    
+    let sum = 0;
+    list.forEach((value) => sum += value);
+    return sum / list.length
+
+}
+
+//Isabel Sample Request
 app.get("/samples/ITR", (req, res) => {
 
-    const result = averageRecoveredExpensesByCountry(isabel_data, "Poland"); 
+    const result = averageRecoveredExpensesByCountry(isabel_data, "Romania"); 
     res.send(`<html> <body> <h1> The average recovered expenses for the chosen country is:  ${result}</h1> </body> </html>`)
     
 });
