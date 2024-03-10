73e71bc85046f53ce809d1cb2bec28ff1d930e65
Isabel María Trapero <isatrarom@alum.us.es>
2024-03-10 10:42:28 +0100

    últimos cambios

--- api-structural-payment-data/index.js
+++ api-structural-payment-data/index.js
@@ -10,33 +10,61 @@ module.exports = (app, db) => {
     
     });
 
-    //GET - PAGINACIÓN Y BÚSQUEDAD POR CAMPOS
+    //GET - PAGINACIÓN, BÚSQUEDAD POR CAMPOS Y PERIODO
     app.get(API_BASE + "/structural-payment-data", (req, res) => {
         const queryParameters = req.query;
         const limit = parseInt(queryParameters.limit) || 10;
         const offset = parseInt(queryParameters.offset) || 0;
-        const hasSearchParameters = Object.keys(queryParameters).some(key => key !== 'limit' && key !== 'offset');
-        let query = {};
+        let from = req.query.from;
+        let to = req.query.to;
     
-        // Si no se proporcionaron parámetros de búsqueda, verificamos si hay datos en la base de datos
+        // Verifica si hay parámetros 'from' y 'to'
+        if (from !== undefined && to !== undefined) {
+            const fromYear = parseInt(from);
+            const toYear = parseInt(to);
+            console.log(fromYear, toYear);
+            if (isNaN(fromYear) || isNaN(toYear)) {
+                return res.status(400).send("Invalid year format. Please provide valid year values.");
+            }
+            // Si los años son válidos, construye la consulta para filtrar por el rango de años
+            queryParameters.year = { $gte: fromYear, $lte: toYear };
+        }
+    
+        let query = {};
+        // Construir la consulta basada en los parámetros proporcionados
+        Object.keys(queryParameters).forEach(key => {
+            if (key !== 'limit' && key !== 'offset' && key !== 'from' && key !== 'to') {
+                const value = !isNaN(queryParameters[key]) ? parseFloat(queryParameters[key]) : queryParameters[key];
+                if (typeof value === 'string') {
+                    query[key] = new RegExp(value, 'i');
+                } else {
+                    query[key] = value;
+                }
+            }
+        });
+    
+        // Verificar si se proporcionaron parámetros de búsqueda
+        const hasSearchParameters = Object.keys(queryParameters).some(key => key !== 'limit' && key !== 'offset' && key !== 'from' && key !== 'to');
+    
         if (!hasSearchParameters) {
             db.count({}, (err, count) => {
                 if (err) {
                     res.sendStatus(500);
                 } else {
                     if (count === 0) {
-                        // Si no hay datos, devolvemos una lista vacía
+                        console.error("If there is no data, we return an empty list.");
                         res.status(200).json([]);
                     } else {
-                        // Si hay datos, realizamos la consulta para obtener todos los datos
                         db.find({}).skip(offset).limit(limit).exec((err, data) => {
                             if (err) {
-                                res.sendStatus(500, "Internal Error");
+                                console.error("Error when inserting data:", err);
+                                res.sendStatus(500);
                             } else {
                                 const resultsWithoutId = data.map(d => {
                                     const { _id, ...datWithoutId } = d;
                                     return datWithoutId;
                                 });
+                                console.log("Sending the data");
                                 res.status(200).json(resultsWithoutId);
                             }
                         });
@@ -44,32 +72,21 @@ module.exports = (app, db) => {
                 }
             });
         } else {
-            // Construimos la consulta basada en los parámetros proporcionados
-            Object.keys(queryParameters).forEach(key => {
-                if (key !== 'limit' && key !== 'offset') {
-                    const value = !isNaN(queryParameters[key]) ? parseFloat(queryParameters[key]) : queryParameters[key];
-                    if (typeof value === 'string') {
-                        query[key] = new RegExp(value, 'i');
-                    } else {
-                        query[key] = value;
-                    }
-                }
-            });
-    
-            // Ejecutamos la consulta en la base de datos con paginación
             db.find(query).skip(offset).limit(limit).exec((err, data) => {
                 if (err) {
-                    res.sendStatus(500, "Internal Error");
+                    console.error("Database error:", err);
+                    res.status(500).send("Internal Server Error");
+                    return;
+                }
+                if (data.length > 0) {
+                    const formattedData = data.map((d) => {
+                        const { _id, ...formatted } = d;
+                        return formatted;
+                    });
+                    res.status(200).json(formattedData);
                 } else {
-                    if (data.length === 0) {
-                        res.status(404).send("Not Found");
-                    } else {
-                        const resultsWithoutId = data.map(d => {
-                            const { _id, ...datWithoutId } = d;
-                            return datWithoutId;
-                        });
-                        res.status(200).json(resultsWithoutId);
-                    }
+                    console.error("Datos no existentes");
+                    res.status(404).send("Not Found");
                 }
             });
         }
@@ -95,7 +112,60 @@ module.exports = (app, db) => {
             }
         });
     });
+
+   //GET - ACCEDER A UN DATO CONCRETO
+   app.get(API_BASE + "/structural-payment-data/:ms_name/:fund", (req, res) => {
+    let pais = req.params.ms_name;
+    let fund = req.params.fund;
+
+    db.find({ ms_name: pais, fund:fund }, (err, countryData) => {
+        if (err) {
+            console.error("Database error:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+        if (countryData.length > 0) {
+            const formattedData = countryData.map((c) => {
+                const { _id, ...formatted } = c;
+                return formatted;
+            });
+            res.status(200).json(formattedData); // Devolver los datos encontrados
+        } else {
+            console.error("Datos no existentes");
+            res.status(404).send("Not Found");
+        }
+    });
+});
+
+    //GET - DATOS DE UN PAIS EN UN PERIODO DE TIEMPO
+    app.get(API_BASE + "/structural-payment-data/:ms_name", (req, res) => {
+
+        let pais = req.params.ms_name;
+        let fromDate = req.query.from;
+        let toDate = req.query.to;
     
+        let query = { ms_name: pais };
+        if (fromDate && toDate) {
+            query.year = { $gte: parseInt(fromDate), $lte: parseInt(toDate) };
+        }
+    
+        db.find(query, (err, countryData) => {
+            if (err) {
+                res.status(500).json({ error: "Internal Server Error" });
+                return;
+            }
+            if (countryData.length > 0) {
+                const formattedData = countryData.map((c) => {
+                    const { _id, ...formatted } = c;
+                    return formatted;
+                });
+                res.status(200).json(formattedData);
+            } else {
+                res.sendStatus(404, "Not Found");
+            }
+        });
+    });
+
 
     //POST - VERIFICAR SI LOS DATOS YA EXISTEN EN LA BASE DE DATOS
     app.post(API_BASE + "/structural-payment-data", (req, res) => {
@@ -178,24 +248,31 @@ module.exports = (app, db) => {
         res.sendStatus(405, "Method Not Allowed");
     });
 
-    //PUT - ACTUALIZAR DATO DE UN PAIS
-    app.put(API_BASE+"/structural-payment-data/:ms_name/:fund", (req,res) => {
-        let country = req.params.ms_name;
-        let fund = req.params.fund;
+    //PUT - ACTUALIZAR UN DATO CONCRETO
+    // PUT --- OK statistics
+    app.put(API_BASE + "/structural-payment-data/:ms_name/:fund", (req, res) => { 
+        const fund = req.params.fund; // El atributo cci es un atributo único por dato.
+        const ms_name = req.params.ms_name; // Obtener ms_name de los parámetros de la ruta
         let data = req.body;
-        if (!data || Object.keys(data).length === 0 || data.ms_name !== country || data.fund !== fund) {
-            res.sendStatus(400, "Bad request");
-        } else {
-            db.update({ "ms_name": country, "fund": fund }, {data}, (err) => {
-                if (err) {
-                    res.sendStatus(500, "Internal Server Error"); // Error interno del servidor
-                }
-                res.sendStatus(200, "Ok"); //Actualizacion correcta
-            });
+    
+        if (!data || Object.keys(data).length === 0 || data.fund !== fund || data.ms_name !== ms_name) {
+            console.error("Invalid data");
+            res.status(400).send("Bad Request"); 
+            return;
         }
-    });
+    
+        db.update({ ms_name: ms_name, fund: fund }, data, {}, (err) => {
+            if (err) {
+                console.error("Database error:", err);
+                res.status(500).send("Internal Server Error"); 
+            } else {
+                console.log("Correct update");
+                res.status(200).send("Ok"); 
+            }
+        });
+    });
 
-    //DELETE - BORRA DATOS DE UN PAIS
+    //DELETE - ELIMINAR UN DATO CONCRETO
     app.delete(API_BASE + "/structural-payment-data/:ms_name/:fund", (req, res) => {
         let country = req.params.ms_name;
         let fund = req.params.fund;
