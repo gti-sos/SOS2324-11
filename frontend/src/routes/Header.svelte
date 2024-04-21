2474160311fa950bfdc8ea5cf29255346b033889
marketes03 <alvmardel4@alum.us.es>
2024-04-21 12:43:54 +0200

    graficas hechas

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -14,7 +14,7 @@
                     <a href="/analytics">Gráfica grupal</a>
                     <a href="/structural-investment/vista">Inversión estructural</a>
                     <a href="/structural-payment-data/vista">Pagos estructurales</a>
-                    <a href="/vista3">Rastreador de socioeconomía</a>
+                    <a href="/socioeconomics-tracker-using-unconventional-data/vista">Rastreador de socioeconomía</a>
                 </div>
             </div>
         </div>
