584c6e41733eb93fb32ab92359d109adb86a2f8e
shaporjur <shaporjur@alum.us.es>
2024-04-26 12:22:25 +0200

    Cambios

--- frontend/src/routes/analytics/+page.svelte
+++ frontend/src/routes/analytics/+page.svelte
@@ -22,100 +22,98 @@
   }
 
 
-async function processCountryData() {
-    const data1 = await fetchData(apiUrl1);
-    console.log(data1);
-    const data2 = await fetchData(apiUrl2);
-    console.log(data2);
-    const data3 = await fetchData(apiUrl3);
-    console.log(data3);
+  async function processCountryData() {
+    
+      const data1 = await fetchData(apiUrl1);
+      console.log(data1);
+      const data2 = await fetchData(apiUrl2);
+      console.log(data2);
+      const data3 = await fetchData(apiUrl3);
+      console.log(data3);
 
-    const combinedData = {};
+      const combinedData = {};
 
-    // Combinar datos de las tres fuentes
-    [data1, data2, data3].forEach(data => {
-        data.forEach(entry => {
-            const country = entry.ms_name || entry.country; // Asegurarse de que se utilice el campo correcto para el país
-            if (!combinedData[country]) {
-                combinedData[country] = {
-                    name: country,
-                    net_planned_eu_amount: 0,
-                    total_net_payment: 0,
-                    popularity_rate: 0,
-                };
-            }
+      // Combinar datos de las tres fuentes
+      [data1, data2, data3].forEach(data => {
+          data.forEach(entry => {
+              const country = entry.ms_name || entry.country; // Asegurarse de que se utilice el campo correcto para el país
+              if (!combinedData[country]) {
+                  combinedData[country] = {
+                      name: country,
+                      net_planned_eu_amount: 0,
+                      total_net_payment: 0,
+                      popularity_rate: 0,
+                  };
+              }
 
-            combinedData[country].net_planned_eu_amount += entry.net_planned_eu_amount || 0;
-            combinedData[country].total_net_payment += entry.total_net_payment || 0;
-            combinedData[country].popularity_rate += entry.popularity_rate || 0;
-        });
-    });
+              combinedData[country].net_planned_eu_amount += entry.net_planned_eu_amount || 0;
+              combinedData[country].total_net_payment += entry.total_net_payment || 0;
+              combinedData[country].popularity_rate += entry.popularity_rate || 0;
+          });
+      });
 
-    // Convertir el objeto combinedData en un array para facilitar su uso en la gráfica
-    countryData = Object.values(combinedData);
+      // Convertir el objeto combinedData en un array para facilitar su uso en la gráfica
+      countryData = Object.values(combinedData);
 
-    // Establecer dataAvailable basado en si hay datos disponibles
-    dataAvailable = countryData.length > 0;
-}
-
-
-function createChart() {
-  // Calcular los valores mínimos y máximos para cada conjunto de datos
-  const minNetPlannedEuAmount = Math.min(...countryData.map(country => country.net_planned_eu_amount));
-  const maxNetPlannedEuAmount = Math.max(...countryData.map(country => country.net_planned_eu_amount));
-
-  const minTotalNetPayment = Math.min(...countryData.map(country => country.total_net_payment));
-  const maxTotalNetPayment = Math.max(...countryData.map(country => country.total_net_payment));
-
-  const minPopularityRate = Math.min(...countryData.map(country => country.popularity_rate));
-  const maxPopularityRate = Math.max(...countryData.map(country => country.popularity_rate));
-
-  Highcharts.chart('chart-container', {
-      chart: {
-        type: 'column'
-      },
-      title: {
-        text: 'Datos combinados por país'
-      },
-      xAxis: {
-        categories: countryData.map(country => country.name),
-        title: {
-          text: 'País'
-        }
-      },
-      yAxis: [{
-        title: {
-          text: 'Net Planned EU Amount y Total Net Payment'
-        },
-        min: Math.min(minNetPlannedEuAmount, minTotalNetPayment), 
-        max: Math.max(maxNetPlannedEuAmount, maxTotalNetPayment), 
-        opposite: false // Este eje Y estará a la izquierda
-      }, {
-        title: {
-          text: 'Popularity Rate'
-        },
-        min: minPopularityRate, 
-        max: maxPopularityRate, 
-        opposite: true // Este eje Y estará a la derecha
-      }],
-      series: [{
-        name: 'Net Planned EU Amount',
-        data: countryData.map(country => country.net_planned_eu_amount),
-        yAxis: 0 // Asignar a este eje Y
-      }, {
-        name: 'Total Net Payment',
-        data: countryData.map(country => country.total_net_payment),
-        yAxis: 0 // Asignar a este eje Y
-      }, {
-        name: 'Popularity Rate',
-        data: countryData.map(country => country.popularity_rate),
-        yAxis: 1 // Asignar a este eje Y
-      }]
-  });
+      // Establecer dataAvailable basado en si hay datos disponibles
+      dataAvailable = countryData.length > 0;
   }
 
 
+  function createChart() {
+    // Calcular los valores mínimos y máximos para cada conjunto de datos
+    const minNetPlannedEuAmount = Math.min(...countryData.map(country => country.net_planned_eu_amount));
+    const maxNetPlannedEuAmount = Math.max(...countryData.map(country => country.net_planned_eu_amount));
 
+    const minTotalNetPayment = Math.min(...countryData.map(country => country.total_net_payment));
+    const maxTotalNetPayment = Math.max(...countryData.map(country => country.total_net_payment));
+
+    const minPopularityRate = Math.min(...countryData.map(country => country.popularity_rate));
+    const maxPopularityRate = Math.max(...countryData.map(country => country.popularity_rate));
+
+    Highcharts.chart('chart-container', {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Datos combinados por país'
+        },
+        xAxis: {
+          categories: countryData.map(country => country.name),
+          title: {
+            text: 'País'
+          }
+        },
+        yAxis: [{
+          title: {
+            text: 'Net Planned EU Amount y Total Net Payment'
+          },
+          min: Math.min(minNetPlannedEuAmount, minTotalNetPayment), 
+          max: Math.max(maxNetPlannedEuAmount, maxTotalNetPayment), 
+          opposite: false // Este eje Y estará a la izquierda
+        }, {
+          title: {
+            text: 'Popularity Rate'
+          },
+          min: minPopularityRate, 
+          max: maxPopularityRate, 
+          opposite: true // Este eje Y estará a la derecha
+        }],
+        series: [{
+          name: 'Net Planned EU Amount',
+          data: countryData.map(country => country.net_planned_eu_amount),
+          yAxis: 0 // Asignar a este eje Y
+        }, {
+          name: 'Total Net Payment',
+          data: countryData.map(country => country.total_net_payment),
+          yAxis: 0 // Asignar a este eje Y
+        }, {
+          name: 'Popularity Rate',
+          data: countryData.map(country => country.popularity_rate),
+          yAxis: 1 // Asignar a este eje Y
+        }]
+    });
+    }
 
   async function insertData() {
     try {
