f3ed81dfbe5a9c9a46d170d3f15c18f106a51282
shaporjur <shaporjur@alum.us.es>
2024-05-05 22:03:08 +0200

    Integracion sin proxy

--- frontend/src/routes/integrations/structural-investment/integrations/sinProxy/+page.svelte
+++ frontend/src/routes/integrations/structural-investment/integrations/sinProxy/+page.svelte
@@ -1,145 +1,163 @@
 <svelte:head>
+
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
 </svelte:head>
 
 <script>
-    import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
 
-    let data1 = [];
-    let data2 = [];
-    let dataAvailable = false;
+  let dataAvailable = false; 
+  const apiUrl1 = 'https://sos2324-11.appspot.com/api/v2/structural-investment-data';
+  const apiUrl2 = 'https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?s=Soccer&c=Spain';
+  let yearData = [];
 
-    async function fetchData1() {
-      try {
-        const response = await fetch('https://sky-scanner3.p.rapidapi.com/get-config', {
-          method: 'GET',
-          headers: {
-            'X-RapidAPI-Key': 'dfc6d02a2emsh256a933352cbeb3p1422d0jsn488d0fd24824',
-            'X-RapidAPI-Host': 'sky-scanner3.p.rapidapi.com'
-          }
-        });
-        const jsonData = await response.json();
-        data1 = jsonData;
-        console.log('Datos de la primera API:', data1);
-      } catch (error) {
-        console.error('Error al obtener datos de la primera API:', error);
-      }
+  async function fetchData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('Data fetched from', url, data);
+    return data;
+  }
+
+  async function combinedData() {
+    const data1 = await fetchData(apiUrl1);
+    const data2 = await fetchData(apiUrl2);
+
+    // Verificar si hay datos disponibles en ambas fuentes
+    if (!Array.isArray(data1) || !data2 || !Array.isArray(data2.teams)) {
+        console.error('No se pudieron obtener los datos de ambas fuentes.');
+        return;
     }
+
+    const combinedData = {};
+
+    if (data1.length === 0) {
+        dataAvailable = false;
+      } else {
+        dataAvailable=true;
+
+    // Iterar sobre los datos de la URL1 y construir un conjunto de años
+    const yearsFromData1 = new Set(data1.map(entry => parseInt(entry.year , 10)));
+    console.log("Años url1", yearsFromData1)
+    const yearsFromData2 = new Set(data2.teams.map(team => parseInt(team.intFormedYear, 10)));
+    console.log("Años url2", yearsFromData2)
+
+    //Encomtarr años en comun
+    const commonYears = Array.from(new Set([...yearsFromData1].filter(year => yearsFromData2.has(year))));
+    console.log("Años comun", commonYears)
+
+       // Iterar sobre los años comunes y obtener los datos correspondientes de cada URL
+       commonYears.forEach(year => {
+            const dataFromUrl1 = data1.find(entry => entry.year === year );
+            const dataFromUrl2 = data2.teams.find(team => team.intFormedYear === year.toString());
+            console.log("1", dataFromUrl1)
+            console.log("2", dataFromUrl2)
+            if (dataFromUrl1 && dataFromUrl2) {
+                // Asegurarse de que combinedData sea un objeto para poder agregar propiedades
+                if (!combinedData[year]) {
+                    combinedData[year] = {
+                        year: year,
+                        net_planned_eu_amount: 0,
+                        idAPIfootball: 0,
+                    };
+                }
+                combinedData[year].net_planned_eu_amount = dataFromUrl1.net_planned_eu_amount || 0;
+                combinedData[year].idAPIfootball += dataFromUrl2.idAPIfootball || 0;
+        
+            }
+        });
+
+    
+  }
+
+    // Filtrar solo los años comunes con datos en ambas fuentes
+    yearData = Object.values(combinedData);
+    console.log("Datos combinados" , yearData);
   
-    async function fetchData2() {
-      try {
-        const response = await fetch('https://sos2324-11.appspot.com/api/v2/structural-investment-data');
-        const jsonData = await response.json();
-        data2 = jsonData;
-        console.log('Datos de la segunda API:', data2);
-      } catch (error) {
-        console.error('Error al obtener datos de la segunda API:', error);
-      }
-    }
-
-    async function loadData() { 
-    try {
-        const response = await fetch('https://sos2324-11.appspot.com/api/v2/structural-investment-data/loadInitialData', {
-            method: 'GET',
-        });
-
-        const status = response.status;
-        console.log(`Status code: ${status}`);
-
-        if (status === 200) { 
-            await fetchData1(); 
-            
-            if (data2.length > 0) {
-                dataAvailable = true; 
-                combineData()
-                renderChart();
-            } 
-        } 
-    } catch (error) {
-        console.error(error);
-    }
 }
 
 
-    async function getData() {
-
-        try {
-            const res = await fetch('https://sos2324-11.appspot.com/api/v2/structural-investment-data');
-            const data = await res.json();
-            console.log(`Data received: ${JSON.stringify(data, null, 2)}`);
-   
-            if (data.length > 0) {
-                dataAvailable = true; 
-                renderChart();
-            }
-
-        } catch (error) {
-            console.log(`Error fetching data: ${error}`);
-        }
-    }
-    
-    function combineData() {
-        const combinedNames = new Set([...(data1.data || []).map(country => country.country), ...(data2 || []).map(country => country.ms_name)]);
-        console.log("Combined Names:", combinedNames);
-        
-        const combinedData = Array.from(combinedNames).map(name => {
-            const country1 = (data1.data || []).find(country => country.country === name);
-            const country2 = (data2.data || []).find(country => country.ms_name === name);
-            console.log("Country 1:", country1);
-            console.log("Country 2:", country2);
-            
-            return {
-                name: name,
-                value1: country1 ? country1.market : 0,
-                value2: country2 ? country2.cci : 0
-            };
-        });
-        console.log("Combined Data:", combinedData);
-        
-        return combinedData;
-    }
-
-
-
-  function renderChart() {
-    const dataCombined = combineData();
+function createChart() {
+    if (!dataAvailable) return; 
+    if (!yearData || yearData.length === 0) return;
 
     Highcharts.chart('chart-container', {
-      title: {
-        text: 'Datos combinados por país'
-      },
-      xAxis: {
-        categories: dataCombined.map(country => country.name)
-      },
-      yAxis: {
+        chart: {
+            type: 'column'
+        },
         title: {
-          text: 'Valor'
-        }
-      },
-      series: [{
-        name: 'Data 1',
-        data: dataCombined.map(country => country.value1)
-      }, {
-        name: 'Data 2',
-        data: dataCombined.map(country => country.value2)
-      }]
+            text: 'Datos combinados por año'
+        },
+        xAxis: {
+            categories: yearData.map(data => data.year)
+        },
+        yAxis: [{
+            title: {
+                text: 'Popularity Rate'
+            }
+        }, {
+            title: {
+                text: 'ID API Football'
+            },
+            opposite: true
+        }],
+        series: [{
+            name: 'Popularity Rate',
+            data: yearData.map(data => data.net_planned_eu_amount),
+            yAxis: 0,
+            dataLabels: {
+                enabled: true,
+                format: '{y}'
+            }
+        }, {
+            name: 'ID API Football',
+            data: yearData.map(data => parseInt(data.idAPIfootball)),
+            yAxis: 1,
+            dataLabels: {
+                enabled: true,
+                format: '{y}'
+            },
+            pointWidth: 40 // Ajustar el ancho de la columna de la serie "ID API Football"
+        }]
     });
+}
+
+
+
+  async function insertData() {
+    try {
+      let response = await fetch(apiUrl1 + "/loadInitialData", {
+        method: "GET",
+      });
+      let status = await response.status;
+      console.log(`Status code: ${status}`);
+      if (status === 200) {
+        await combinedData();
+        if (dataAvailable) {
+          createChart();
+        }
+      } 
+    } catch (e) {
+      console.error(e);
+    }
   }
-
-
+    
   
     onMount(async () => {
-      await getData();
+      await combinedData();
+    if (dataAvailable) {
+      createChart();
+    }
     });
 
 </script>
   
 {#if dataAvailable==false}
   <e>No hay datos disponibles. Por favor, introduzca los datos.</e>
-  <button class="initial" on:click={loadData}>Cargar datos de prueba</button>
+  <button class="initial" on:click={insertData}>Cargar datos de prueba</button>
   {/if}
 <div id="chart-container" />
 
