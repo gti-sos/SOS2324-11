ed9443438e7b39f97307c7a6fd5b122336e1bf5f
Isabel María Trapero <isatrarom@alum.us.es>
2024-05-06 08:30:40 +0200

    uso api

---
+++ frontend/src/routes/integrations/structural-payment/+page.svelte
@@ -0,0 +1,39 @@
+<div class="container">
+
+    <div class="card">
+        <div class="photo">
+            <a href="http://localhost:5173/integrations/structural-payment/uses"><img src = "https://bitgenoma.com/web/image/17134" alt="Imagen" style="height: 450px;"></a>
+        </div>
+        <h2 class="photo-title">Usos</h2>
+    </div>
+
+    <div class="card" style="margin-left: 20px;">
+        <div class="photo">
+            <a href="http://localhost:5173/integrations/structural-payment/integrations"><img src = "https://www.varadero.es/wp-content/uploads/2020/03/servcios-it-integracion-software.png" alt="Imagen"  style="height: 450px;"></a>
+        </div>
+        <h2 class="title">Integraciones</h2>
+    </div>  
+
+</div> 
+
+
+
+<style>
+
+   .container{
+       display: grid;
+       grid-template-columns: 50% 50%;
+   }
+   
+   div.card {
+       text-align: center;
+       border: 1px solid gray;
+       border-radius: 5%;
+   }
+
+   img {
+       max-height: 400px;
+       width: auto;
+   }
+
+</style>
---
+++ frontend/src/routes/integrations/structural-payment/uses/+page.svelte
@@ -0,0 +1,52 @@
+<div class="container">
+
+    <div class="card">
+        <div class="photo">
+            <a href="http://localhost:5173/integrations/structural-payment/uses/ejercicio"><img class="imgA" src = "https://png.pngtree.com/png-vector/20191027/ourlarge/pngtree-hotel-building-vector-illustration-with-simple-design-hotel-icon-png-image_1850754.jpg" alt="Imagen"></a>
+        </div>
+        <h2 class="title">Ejercicio</h2>
+    </div>
+
+    <div class="card" style="margin-left: 20px;">
+        <div class="photo">
+            <a href="http://localhost:5173/integrations/structural-payment/uses/comida"><img class="imgP" src = "https://i.pinimg.com/736x/b1/b2/78/b1b27841d268d0dcf203473b7b5da5f3.jpg" alt="Imagen"></a>
+        </div>
+        <h2 class="title">Comida</h2>
+    </div>
+
+    <div class="card" style="margin-left: 20px;">
+        <div class="photo">
+            <a href="http://localhost:5173/integrations/structural-payment/uses/geografia"><img class="imgP"src = "https://static.vecteezy.com/system/resources/previews/027/511/289/original/cartoon-hand-holding-united-states-and-canadian-flags-usa-and-canada-relations-vector.jpg" alt="Imagen"></a>
+        </div>
+        <h2 class="title">Geografia</h2>
+    </div>
+    
+</div> 
+
+
+<style>
+
+    .container{
+        display: grid;
+        grid-template-columns: 33% 33% 33%;
+    }
+
+    div.card {
+        text-align: center;
+        border: 1px solid gray;
+        border-radius: 5%;
+    }
+
+
+    .imgA {
+        height: 280px;
+        margin-top: 20px;
+    }
+
+    .imgP {
+        height: 260px;
+        margin-top: 20px;
+    }
+
+
+</style>
