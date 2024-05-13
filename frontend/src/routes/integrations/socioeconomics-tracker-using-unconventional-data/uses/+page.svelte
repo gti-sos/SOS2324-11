c6c94ca057e3e52f766a6b47fb7a35a69d3fda4f
marketes03 <alvmardel4@alum.us.es>
2024-05-13 21:45:23 +0200

    cambio de local a nube

--- frontend/src/routes/integrations/socioeconomics-tracker-using-unconventional-data/uses/+page.svelte
+++ frontend/src/routes/integrations/socioeconomics-tracker-using-unconventional-data/uses/+page.svelte
@@ -2,21 +2,21 @@
 
     <div class="card">
         <div class="photo">
-            <a href="http://localhost:5173/integrations/socioeconomics-tracker-using-unconventional-data/uses/spotify"><img class="imgA" src = "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/03/Spotify-Logo-Green-Black.jpg?resize=1500%2C1000&quality=55&strip=all&ssl=1" alt="Imagen"></a>
+            <a href="http://sos2324-11.appspot.com/integrations/socioeconomics-tracker-using-unconventional-data/uses/spotify"><img class="imgA" src = "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/03/Spotify-Logo-Green-Black.jpg?resize=1500%2C1000&quality=55&strip=all&ssl=1" alt="Imagen"></a>
         </div>
         <h2 class="title">Spotify</h2>
     </div>
 
     <div class="card" style="margin-left: 20px;">
         <div class="photo">
-            <a href="http://localhost:5173/integrations/socioeconomics-tracker-using-unconventional-data/uses/futbol"><img class="imgP" src = "https://assets.goal.com/images/v3/blt3a1064b1b3d4f11f/5a78219590785167909a9df0b82a7a5b0ece8f8d.jpg?auto=webp&format=pjpg&width=1200&quality=60" alt="Imagen"></a>
+            <a href="http://sos2324-11.appspot.com/integrations/socioeconomics-tracker-using-unconventional-data/uses/futbol"><img class="imgP" src = "https://assets.goal.com/images/v3/blt3a1064b1b3d4f11f/5a78219590785167909a9df0b82a7a5b0ece8f8d.jpg?auto=webp&format=pjpg&width=1200&quality=60" alt="Imagen"></a>
         </div>
         <h2 class="title">Goleadores Premier</h2>
     </div>
 
     <div class="card" style="margin-left: 20px;">
         <div class="photo">
-            <a href="http://localhost:5173/integrations/socioeconomics-tracker-using-unconventional-data/uses/imdb"><img class="imgR"src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/330px-IMDB_Logo_2016.svg.png" alt="Imagen"></a>
+            <a href="http://sos2324-11.appspot.com/integrations/socioeconomics-tracker-using-unconventional-data/uses/imdb"><img class="imgR"src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/330px-IMDB_Logo_2016.svg.png" alt="Imagen"></a>
         </div>
         <h2 class="title">Top 10 peliculas semanales</h2>
     </div>
