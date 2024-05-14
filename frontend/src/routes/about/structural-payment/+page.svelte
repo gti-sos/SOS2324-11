1055b3a7e6c624a0c339a344548fee6f8986f9e8
Isabel María Trapero <isatrarom@alum.us.es>
2024-05-14 16:54:08 +0200

    Vídeo Isabel + toggl

---
+++ frontend/src/routes/about/structural-payment/+page.svelte
@@ -0,0 +1,26 @@
+
+<style>
+    .video-container {
+     display: flex;
+    justify-content: center;
+   align-items: center;
+    height: 100vh;
+  }
+    
+    .video-container iframe {
+    width: 1000px;
+    height: 450px;
+    /* Puedes ajustar el tamaño del video aquí */
+   }
+   
+     @media (max-width: 600px) {
+    .video-container iframe {
+   width: 100%;
+  height: auto;
+     }
+    }
+</style>
+    
+<div class="video-container">
+     <iframe src="https://www.youtube.com/embed/FpO9lrKLzLs?si=iQHdYdleIjYyS4EJ" title="Vídeo Isabel" frameborder="0" allowfullscreen></iframe>
+</div>
