e9f1fb2d169e6a6712ff895dcbe73d762e05ffeb
shaporjur <shaporjur@alum.us.es>
2024-05-14 21:08:21 +0200

    Videos

---
+++ frontend/src/routes/about/structural-investment/+page.svelte
@@ -0,0 +1,29 @@
+
+<style>
+
+  .video-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+    
+  .video-container iframe {
+    width: 1450px;
+    height: 590px;
+  }
+   
+  @media (max-width: 600px) {
+
+    .video-container iframe {
+      width: 100%;
+      height: auto;
+        }
+
+  }
+
+</style>
+    
+<div class="video-container">
+  <iframe src="https://www.youtube.com/embed/ND3MPNXzfEk" title="Vídeo Sharay" frameborder="0" allowfullscreen></iframe>
+</div>
